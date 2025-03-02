572ea8e5ddaccc73ed52790afc0eebe032300c05
Devashish Chakraborty <devashishchakra@gmail.com>
2025-03-02 17:47:42 +0530

    Organised the code into different files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import routes from "./routes/index.js";
+import authenticateJWT from "./middlewares/authenticateToken.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,7 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(import.meta.dirname, "public")));
 
-app.use("/posts", routes.postsRouter);
+app.use(routes.authRouter)
+app.use("/posts", authenticateJWT, routes.postsRouter);
 
 app.listen(port, () => {
   console.log(`Blog API listening on port ${port}`);
