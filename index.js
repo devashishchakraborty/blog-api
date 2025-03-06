6b26271dd80d96c4ef7f76d9e5f5c4625743c834
Devashish Chakraborty <devashishchakra@gmail.com>
2025-03-07 02:16:32 +0530

    added user route to get user

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(cors());
 
 app.use(routes.authRouter);
 app.use("/posts", authenticateJWT, routes.postRouter);
+app.get("/user", authenticateJWT, (req, res) => res.send(req.user));
 
 app.use((err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
--- routes/profileRouter.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import profileController from "../controllers/profileController.js";
-const userRouter = Router();
-
-userRouter("/", profileController.getProfile);
